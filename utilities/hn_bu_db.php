f01e99b95b7777cfb3758e7b80105433b14b8c1f
Jesse Blum <jesse.blum@nottingham.ac.uk>
2012-12-10 16:44:53 +0000

    Updated to support single site.

--- utilities/hn_bu_db.php
+++ utilities/hn_bu_db.php
@@ -3,6 +3,20 @@
  * Class to interact with the wp_ts database tables
  * Author: Jesse Blum (JMB)
  * Date: 2012
+ * Copyright (C) 2012  Horizon Digital Economy Research Institute, Jesse Blum (JMB) & Martin Flintham (MDF)
+	
+	    This program is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU Affero General Public License as
+	    published by the Free Software Foundation, either version 3 of the
+	    License, or (at your option) any later version.
+	
+	    This program is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU Affero General Public License for more details.
+	
+	    You should have received a copy of the GNU Affero General Public License
+	    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');	// provides dbDelta
@@ -10,7 +24,7 @@ require_once(ABSPATH . WPINC . '/class-IXR.php');
 require_once(ABSPATH . WPINC . '/class-wp-xmlrpc-server.php');
 
 /**
- * Controls calls to the database for timestreams
+ * Controls calls to the database for blogsuser
  * @author pszjmb
  *
  */
@@ -76,16 +90,28 @@ class HN_BU_Database {
 		}
 	}
 	
+	/**
+	 * Adds all users and blogs to the database. 
+	 * If they already exist, they will be ignored. 
+	 */
 	function hn_bu_addAllUsersBlogs(){
 		$user_list = $this->hn_bu_getUserlist();
 		$userblogarray = array();
 		$counter=0;
 		foreach ($user_list AS $user) {
-			$user_blogs = get_blogs_of_user( $user->ID );
-			foreach ($user_blogs AS $user_blog) {
-				$userblogarray[$counter]=array($user->ID, $user_blog->site_id, $user_blog->userblog_id);
-				$counter+=1;
-				$this->hn_bu_setUserBloglist($userblogarray);
+			if(is_multisite()){
+				$user_blogs = get_blogs_of_user( $user->ID );
+				foreach ($user_blogs AS $user_blog) {
+					$userblogarray[$counter]=array($user->ID, 
+							$user_blog->site_id, $user_blog->userblog_id);
+					$counter+=1;
+					$this->hn_bu_setUserBloglist($userblogarray);
+				}
+			}else{
+					$this->hn_bu_setUserBloglist($userblogarray);	
+					$userblogarray[$counter]=array($user->ID, 
+							1, 1);
+					$counter+=1;			
 			}
 		}
 	}
