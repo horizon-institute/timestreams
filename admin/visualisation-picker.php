e79de49e46688c1a5cfd095195528b6b4494c8e2
Dominic Price <dominic.price@nottingham.ac.uk>
2017-07-18 15:46:24 +0100

    Modified relative path

--- admin/visualisation-picker.php
+++ admin/visualisation-picker.php
@@ -1,43 +1,43 @@
 <?php
 
-	require_once(  '../../../../wp-load.php');
-	
+	require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+
 	$db = new Hn_TS_Database();
 
 	$timestreams = $db->hn_ts_getTimestreams();
-	
+
 	?>
 	<form name="timestreampicker" id="timestreamForm" action="#">
 	<table class="form-table">
-		
-		
+
+
 		<tr>
 			<th>Select a Timestream</th>
 			<td>
 				<select id="timestream" name="timestream">
 					<option value=""> - Select - </option>
 	<?php
-		
+
 	foreach($timestreams as $timestream)
 	{
 		echo "<option value=\"$timestream->timestream_id\">$timestream->name</option>";
 	}
-	
+
 	?>
 				</select>
 			</td>
 		</tr>
-		
+
 		<tr>
 			<th>Select a Visualisation</th>
 			<td>
 				<select id="viz" name="viz">
 					<option value=""> - Select - </option>
 	<?php
-	
+
 	$visDir = "../visualisations/";
 	$visDirs = glob($visDir . "*", GLOB_ONLYDIR);
-	
+
 	foreach($visDirs as $viz)
 	{
 		$vizName = basename($viz);
@@ -52,7 +52,7 @@
 		</table>
 	</form>
 
-		
+
 		<p class="submit">
 			<input type="button" class="button-primary" value="Insert Visualisation" onclick="insert()" />
 		</p>
